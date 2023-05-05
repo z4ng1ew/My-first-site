10eaa0d2124bca3c240c58e63df70d6226549288
z4ng1ew <123883614+z4ng1ew@users.noreply.github.com>
2023-05-05 14:55:41 +0300

    Delete 5.Учимся вместе directory

--- 5.Учимся вместе/regist.js
+++
@@ -1,6 +0,0 @@
-const regist = document.querySelector('.button');
-const blockEl = document.querySelector('.block');
-regist.addEventListener('click', function (e) {
-    blockEl.classList.toggle('visible')
-
-});
